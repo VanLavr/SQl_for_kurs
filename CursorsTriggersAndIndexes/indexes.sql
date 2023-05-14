d8706d17b9b025bb420e267ccec2d3d28b27de5b
Ivan <lavr.i-n@mail.ru>
2023-05-14 14:02:33 +0300

    debugged indexes

--- CursorsTriggersAndIndexes/indexes.sql
+++ CursorsTriggersAndIndexes/indexes.sql
@@ -1,3 +1,6 @@
+SET enable_seqscan = OFF;
+SET enable_indexscan = ON;
+
 CREATE INDEX airplane_name_idx ON airplanes(airpale_name);
 CREATE UNIQUE INDEX login_idx ON users(login);
 CREATE INDEX fl_idx ON flights(departure_date, arrival_date, departure_city, arrival_city);
